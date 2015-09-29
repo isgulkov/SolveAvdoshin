721b4cd0af8f7ad12517d9f5f133ef369eb2c379
Ilya Gulkov <ifranked@gmail.com>
2015-09-29 15:18:51 +0300

    Implemented some of the bruteforce tools (AllTrees, SetIthVar, SetIthOp)

--- SolveAvdoshin/BooleanFunctions.cs
+++ SolveAvdoshin/BooleanFunctions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SolveAvdoshin
 {
@@ -14,9 +15,17 @@ namespace SolveAvdoshin
 //			Console.WriteLine(f.ToString());
 //			Console.WriteLine(f.ToString("L"));
 
-			var ex = (new BooleanFunction(26)).GetFDNF();
+//			var ex = (new BooleanFunction(26)).GetFDNF();
+//
+//			Console.WriteLine(ex.ToString());
+//
+//			PrintDerivatives(new BooleanFunction(26));
 
-			Console.WriteLine(ex.ToString());
+			foreach(var t in BooleanExpression.AllTrees(3)) {
+				t.SetIthVar(0, BooleanVariable.B);
+				t.setIthOp(0, BooleanOperation.Xor);
+				Console.WriteLine(t.ToString());
+			}
 		}
 	}
 
@@ -33,23 +42,55 @@ namespace SolveAvdoshin
 		{
 			return (TruthTable >> (7 - (a ? 4 : 0) - (b ? 2 : 0) - (c ? 1 : 0)) & 1) == 1;
 		}
+			
+		public int Eval(int a, int b, int c)
+		{
+			return TruthTable >> (7 - a * 4 - b * 2 - c) & 1;
+		}
+
+		public static string VarRows()
+		{
+			string[] lines = new string[] { "A ", "B ", "C ", };
+
+			for(int i = 0; i < 8; i++) {
+				lines[0] += (i >> 2 & 1) + " ";
+				lines[1] += (i >> 1 & 1) + " ";
+				lines[2] += (i & 1) + " ";
+			}
+
+			return String.Join("\n", lines) + "\n";
+		}
+
+		public static string DelimRow()
+		{
+			string line = "--";
+
+			for(int i = 0; i < 8; i++) {
+				line += "--";
+			}
+
+			return line + "\n";
+		}
+
+		public string FuncRow()
+		{
+			string line = "F ";
+
+			for(int i = 0; i < 8; i++) {
+				line += (Eval((i >> 2 & 1) == 1, (i >> 1 & 1) == 1, (i & 1) == 1) ? 1 : 0) + " ";
+			}
+
+			return line + "\n";
+		}
 
 		public string ToString(string format)
 		{
 			if(format == "L") {
-				string[] lines = new string[] { "A ", "B ", "C ", "--", "F ", };
-
-				for(int i = 0; i < 8; i++) {
-					lines[0] += (i >> 2 & 1) + " ";
-					lines[1] += (i >> 1 & 1) + " ";
-					lines[2] += (i & 1) + " ";
-					lines[3] += "--";
-					lines[4] += (Eval((i >> 2 & 1) == 1, (i >> 1 & 1) == 1, (i & 1) == 1) ? 1 : 0) + " ";
-				}
-
 				string result = "";
 
-				foreach(string line in lines) result += line + "\n";
+				result += VarRows();
+				result += DelimRow();
+				result += FuncRow();
 
 				return result;
 			}
@@ -75,22 +116,12 @@ namespace SolveAvdoshin
 					
 				BooleanExpression varA, varB, varC;
 
-				if((i >> 2 & 1) == 1)
-					varA = new VarExpression(BooleanVariable.A);
-				else
-					varA = new NotExpression(BooleanVariable.A);
+				varA = new VarExpression(BooleanVariable.A, (i >> 2 & 1) != 1);
+				varB = new VarExpression(BooleanVariable.B, (i >> 1 & 1) != 1);
+				varC = new VarExpression(BooleanVariable.C, (i & 1) != 1);
 
-				if((i >> 1 & 1) == 1)
-					varB = new VarExpression(BooleanVariable.B);
-				else
-					varB = new NotExpression(BooleanVariable.B);
-
-				if((i & 1) == 1)
-					varC = new VarExpression(BooleanVariable.C);
-				else
-					varC = new NotExpression(BooleanVariable.C);
-
-				cons1 = new OpExpression(BooleanOperation.And, new OpExpression(BooleanOperation.And, varA, varB), varC);
+				cons1 = new OpExpression(BooleanOperation.And,
+					new OpExpression(BooleanOperation.And, varA, varB), varC);
 
 				ex = ex != null ? new OpExpression(BooleanOperation.Or, cons1, ex) : cons1;
 			}
@@ -106,30 +137,35 @@ namespace SolveAvdoshin
 	{
 		abstract public bool Eval(bool a, bool b, bool c);
 		abstract new public string ToString();
-	}
+		abstract public int CountVariables();
+		abstract public void SetIthVar(int i, BooleanVariable value);
+		abstract public int setIthOp(int i, BooleanOperation value);
 
-	class NotExpression : BooleanExpression
-	{
-		BooleanExpression Arg;
-
-		public NotExpression(BooleanExpression arg)
+		public static IEnumerable<BooleanExpression> AllTrees(int size)
 		{
-			Arg = arg;
+			if(size == 0) {
+				yield return new VarExpression(BooleanVariable.A);
+			}
+			else {
+				for(int i = 0; i < size; i++) {
+					foreach(var l in AllTrees(i)) {
+						foreach(var r in AllTrees(size - 1 - i)) {
+							yield return new OpExpression(BooleanOperation.And, l, r);
+						}
+					}
+				}
+			}
 		}
 
-		public NotExpression(BooleanVariable variable)
+		public static IEnumerable<BooleanExpression> AllExpressions(int size, BooleanOperation[] ops,
+			BooleanVariable[] vars)
 		{
-			Arg = new VarExpression(variable);
+			throw new NotImplementedException();
 		}
 
-		public override string ToString()
+		public BooleanExpression FindMininalInBasis()
 		{
-			return "!" + Arg.ToString();
-		}
-
-		public override bool Eval(bool a, bool b, bool c)
-		{
-			return !Arg.Eval(a, b, c);
+			throw new NotImplementedException();
 		}
 	}
 
@@ -215,6 +251,45 @@ namespace SolveAvdoshin
 		{
 			return "(" + Left.ToString() + " " + OpSymbols[(int)Op] + " " + Right.ToString() + ")";
 		}
+
+		public override int CountVariables()
+		{
+			int res = 0;
+
+			res += Left.CountVariables();
+			res += Right.CountVariables();
+
+			return res;
+		}
+
+		public override void SetIthVar(int i, BooleanVariable value)
+		{
+			if(i >= Left.CountVariables()) {
+				Right.SetIthVar(i - Left.CountVariables(), value);
+			}
+			else {
+				Left.SetIthVar(i, value);
+			}
+		}
+
+		public override int setIthOp(int i, BooleanOperation value)
+		{
+			if(i < 0) {
+				return 0;
+			}
+			else {
+				if(i == 0) {
+					Op = value;
+				}
+
+				int res = 1;
+
+				res += Left.setIthOp(i - res, value);
+				res += Right.setIthOp(i - res, value);
+
+				return res;
+			}
+		}
 	}
 
 	class VarExpression : BooleanExpression
@@ -222,21 +297,23 @@ namespace SolveAvdoshin
 		static readonly string[] VarSymbols = new string[] { "A", "B", "C", };
 
 		BooleanVariable Var;
+		bool Not;
 
-		public VarExpression(BooleanVariable var)
+		public VarExpression(BooleanVariable var, bool not = false)
 		{
 			Var = var;
+			Not = not;
 		}
 
 		public override bool Eval(bool a, bool b, bool c)
 		{
 			switch(Var) {
 			case BooleanVariable.A:
-				return a;
+				return a != Not;
 			case BooleanVariable.B:
-				return b;
+				return b != Not;
 			case BooleanVariable.C:
-				return c;
+				return c != Not;
 			default:
 				throw new Exception("Nigga wat?");
 			}
@@ -244,7 +321,22 @@ namespace SolveAvdoshin
 
 		public override string ToString()
 		{
-			return VarSymbols[(int)Var];
+			return (Not ? "!" : "") + VarSymbols[(int)Var];
+		}
+
+		public override int CountVariables()
+		{
+			return 1;
+		}
+
+		public override void SetIthVar(int i, BooleanVariable variable)
+		{
+			this.Var = variable;
+		}
+
+		public override int setIthOp(int i, BooleanOperation value)
+		{
+			return 0;
 		}
 	}
 }
